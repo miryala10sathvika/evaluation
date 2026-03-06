837c163b10bfb1a9c2b3a4b37c60015e7822567b
miryala10sathvika <miryala.sathvika@gmail.com>
2026-03-06 19:49:17 +0530

    adding new line

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    base: '/evaluation', 
+
   build: {
     outDir: 'dist',
   }
